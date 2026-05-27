3b865aab9bb2722c33e4572bd656d955f3e46ded
luizblank <luizrosacardoso@gmail.com>
2026-05-27 09:15:51 -0300

    Project file organization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import UserDetails from "./pages/UserDetails";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/userdetails" element={<UserDetails />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
---
+++ src/pages/UserDetails/index.tsx
@@ -0,0 +1,7 @@
+export default function UserDetails() {
+    return (
+        <>
+            User Details Page
+        </>
+    )
+}
